ce3ae6c56142b4da26732c15c6d10dd07610446b
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2023-01-06 10:04:37 -0300

    basic function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +9,31 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+//FUNCTIONS
+
+function logger() {
+  console.log("My name is Leo");
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(num);
